ab7807a1e78f10ef5a136168f2c12ca693cf1373
SimonGideon <simongideon918@gmail.com>
2021-04-26 14:11:15 +0300

    lst_proV

--- Lists_pro.py
+++ Lists_pro.py
@@ -1,3 +1,5 @@
+import itertools
+
 a = [1, 2, 3, 4, 5]
 a.append(6)
 a.append(7)
@@ -61,4 +63,77 @@ aa = a.copy()
 print(aa)
 
 # Accessing list Values.
+lst = [1, 2, 3, 4]
+print(lst[0])
+print(lst[1:])
+
+# Advance Slicing.
+data = 'chandan purohit 22 200'
+name_slice = slice(0,19)
+age_slice = slice(19, 21)
+salary_slice = slice(22,None)
+
+print(data[name_slice])
+print(data[age_slice])
+print(data[salary_slice])
+lst = []
+if not lst:
+    print("list is empty")
+
+# Iterating Over List.
+my_list = ['foo', 'bar', 'baz']
+for item in my_list:
+    print(item)
+for (index, item) in enumerate(my_list):
+    print('The item in position {} is: {}'.format(index, item))
+
+"""Iterating based on the index value"""
+for i in range(0, len(my_list)):
+    print(my_list)
+
+"""Checking wheitheer an item is the list"""
+list = ['test', 'twest', 'tweast', 'treast']
+print('test' in list)
+
+
+# Any and All.
+nums = [1, 1, 0, 1]
+print(all(nums))
+print(any(nums))
+
+# Reversing list elements.
+
+for i in range(0, 10):
+    print(i,end=" ")
+
+    break
+numbers = [1, 2, 3, 4, 5, 6]
+numbers[::-1]
+print(numbers)
+
+# Concatenation and Merging Lists.
+alist = ['a1', 'a2', 'a3']
+blist = ['b1', 'b2', 'b3']
+for a, b in zip(alist, blist):
+    print(a, b)
+
+alist = []
+"""print(len(list(zip(alist, blist))))"""
+alist = ['a1', 'a2', 'a3']
+blist = ['b1']
+clist = ['c1', 'c2', 'c3', 'c4']
+for a, b, c in itertools.zip_longest(alist, blist, clist):
+    print(a, b, c)
+
+# Length of list.
+print(len(['one', 'two']))
+print(len(['one', [2, 3], 'four']))
+
+# Removing Duplicate Values.
+list():
+name = ["Luke", "Jose", "Edith", "Luke"]
+list(set(name))
+
+
+
 
