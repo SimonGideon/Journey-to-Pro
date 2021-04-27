a5b884ba081563af789d2fefe7b98acd1d555388
SimonGideon <simongideon918@gmail.com>
2021-04-27 12:40:40 +0300

    lst.pro

--- Lists_pro.py
+++ Lists_pro.py
@@ -21,13 +21,18 @@ a.sort(reverse=True)
 print(a)
 
 import datetime
+
+
 class Person(object):
     def __init__(self, name, birthday, height):
         self.name = name
         self.birthday = birthday
         self.height = height
+
     def __repr__(self):
         return self.name
+
+
 a = [Person("John Mark", datetime.date(1992, 9, 12), 175),
      Person("Victor Wong", datetime.date(1990, 8, 28), 180),
      Person("John Skeet", datetime.date(1991, 7, 6), 185)]
@@ -37,16 +42,16 @@ a.sort(key=lambda item: item.height)
 print(a)
 # Way to sort using attrgetter and itemgetter.
 from operator import itemgetter, attrgetter
-people = [{'name':'chandan','age':20,'salary':2000},
-          {'name':'chetan','age':18,'salary':5000},
-          {'name':'guru','age':30,'salary':3000}]
+
+people = [{'name': 'chandan', 'age': 20, 'salary': 2000},
+          {'name': 'chetan', 'age': 18, 'salary': 5000},
+          {'name': 'guru', 'age': 30, 'salary': 3000}]
 by_age = itemgetter('age')
 by_salary = itemgetter('salary')
 people.sort(key=by_age)
 people.sort(key=by_salary)
 
-
-list_of_tuples = [(1,2), (3,4), (5,0)]
+list_of_tuples = [(1, 2), (3, 4), (5, 0)]
 list_of_tuples.sort(key=itemgetter(1))
 print(list_of_tuples)
 
@@ -69,9 +74,9 @@ print(lst[1:])
 
 # Advance Slicing.
 data = 'chandan purohit 22 200'
-name_slice = slice(0,19)
+name_slice = slice(0, 19)
 age_slice = slice(19, 21)
-salary_slice = slice(22,None)
+salary_slice = slice(22, None)
 
 print(data[name_slice])
 print(data[age_slice])
@@ -95,7 +100,6 @@ for i in range(0, len(my_list)):
 list = ['test', 'twest', 'tweast', 'treast']
 print('test' in list)
 
-
 # Any and All.
 nums = [1, 1, 0, 1]
 print(all(nums))
@@ -104,7 +108,7 @@ print(any(nums))
 # Reversing list elements.
 
 for i in range(0, 10):
-    print(i,end=" ")
+    print(i, end=" ")
 
     break
 numbers = [1, 2, 3, 4, 5, 6]
@@ -130,10 +134,74 @@ print(len(['one', 'two']))
 print(len(['one', [2, 3], 'four']))
 
 # Removing Duplicate Values.
-list():
+"""list():
 name = ["Luke", "Jose", "Edith", "Luke"]
-list(set(name))
+list(set(name))"""
 
+# Comparison of list
+n = [1, 10, 100] < [2, 10, 100]
 
+print(n)
+p = [1, 10] < [1, 10, 100]
+print(p)
+
+# Accessing Values in nested list.
+alist = [[[1, 2], [3, 4]], [[5, 6, 7], [8, 9, 10], [12, 13, 14]]]
+print(alist[0][0][1])  # Accessing the second element in the first list in the first list.
+print(alist[1][1][2])  # Accesing the third elemnt in the second list in the secondlist.
+
+# Performing support operations.
+alist[0][0].append(1)
+print(alist[0][0][2])
+
+# Nested for loops.
+for row in alist:  # One way loop through nested list.
+    for col in row:
+        print(col)
+for row in range(len(alist)):
+    for col in range(len(alist[row])):
+        print(alist[row][col])
+# Using slicing.
+print(alist[1][1:])
+print(alist)
+
+# Initializing a list to a fixed number.
+my_list = [None] * 10
+my_list = ['test'] * 10
+
+my_list.append('Name')
+print(my_list)
+
+my_list = [{1}] * 10
+my_list[0].add(2)
+print(my_list)
+
+my_list = [{1} for _ in range(10)]
+print(my_list)
+
+# List Comprehension.
+# Create a list of squares.
+squares = [x * x for x in range(0, 20)]
+print(squares)
+squares = []
+for x in range(0, 20):
+    squares.append(x * x)
+print(squares)
+
+s = "Hello Kenya"
+print(s.upper())
+print([t.upper() for t in "Hello Africa"])
+
+# Strip of many commas from the end of strings in alist.
+print([w.strip(',') for w in ['these,', 'words,,', 'mostly', 'have, commas,']])
+
+# Organize letters in words more responsibly -in alphabetic order.
+sentence = "Handsome is better than ugly"
+print(["".join(sorted(word, key=lambda x: x.lower())) for word in sentence.split()])
+
+from random import  randrange
+print(randrange(1, 7) for _ in range)
+
+# Conditional List Comprehensions.
 
 
