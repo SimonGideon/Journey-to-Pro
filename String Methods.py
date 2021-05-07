1253ee0ab3b36cc14aed481a3c4a6f164556b957
SimonGideon <simongideon918@gmail.com>
2021-05-07 15:32:21 +0300

    str Methods

--- String Methods.py
+++ String Methods.py
@@ -1,1 +1,23 @@
 # Changing the capitalization of strings.
+n="This iS Our Wedding DAy"
+print("this Is A 'String'.".capitalize())
+print(str.swapcase(n))
+print(str.upper(n))
+
+# Translating Character into string.
+translation_table = str.maketrans("aeiou", "12345")
+my_string = "This is a string!"
+translated = my_string.translate(translation_table)
+print(translated)
+
+# String module's useful constants.
+import string
+
+print(string.ascii_uppercase)
+print(string.digits)
+print(string.hexdigits)
+print(string.octdigits)
+print(string.punctuation)
+print(string.whitespace)
+print(string.printable)
+
